dc27d92ac5b8bf52280e3fc8226c7e60647fdcc8
akshayaa-padmanaban <162109563+akshayaa-padmanaban@users.noreply.github.com>
2025-04-25 18:06:41 +0530

    Update Assignments_1.dart

--- Assignments_1.dart
+++ Assignments_1.dart
@@ -13,10 +13,10 @@ void main()
         return;
     }
 
-    if(input.toLowerCase().contains('greater than')||input.toLowerCase().contains('less than'))
+    if(input.startsWith('>')||input.startsWith('<'))
     {
-      String condition=input.toLowerCase();
-      double loanAmount = double.tryParse(condition.split(' ').last)??0;
+      String condition=input[0];
+      double loanAmount = double.tryParse(input.substring(1).trim())??0;
       if(loanAmount==0)
       {
         print('Invalid');
@@ -33,6 +33,14 @@ void main()
        print(match);
       }
     }
+
+    if(input == 'last week' || input == 'last month' || input == 'last quarter')
+    {
+      List<Map<String, dynamic>> match = filterByDate(input);
+      print(match);
+      return;
+    }
+
     else
     {
       bool isfound = false;
@@ -44,32 +52,60 @@ void main()
             {
                 print(data);
                 isfound = true;
-                break;
+                return;
             }
         }
-        if (isfound) break;
+        if (isfound) 
+        return;
       }
       if(!isfound)
-    {
-      print("enter the correct value");
-      return;
+      {
+        print("enter the correct value");
+        return;
+      }
     }
-    }
-}
+  }
 
-List<Map<String, dynamic>> filterBySearchQuery(String condition, double loanAmount) {
+List<Map<String, dynamic>> filterBySearchQuery(condition, double loanAmount) {
   List<Map<String,dynamic>> match = mobCustData.where((data)
   {
     double loanAmt = double.tryParse(data['lcdLoanAmt'].toString())??0;
-    if(condition.contains('greater than')&&loanAmt>loanAmount)
-    {
-      return true;
-    }
-    else if(condition.contains('less than')&&loanAmt<loanAmount)
-    {
-      return true;
-    }
+    if(condition == '>' && loanAmt>loanAmount) return true;
+    else if(condition == '<' && loanAmt<loanAmount) return true;
     return false;
   }).toList();
   return match;
 }
+
+List<Map<String, dynamic>> filterByDate(String dates) {
+  DateTime now = DateTime.now();
+  Duration range;
+
+  switch(dates)
+  {
+    case 'last week':
+    range = Duration(days:7);
+    break;
+
+    case 'last month':
+    range = Duration(days:30);
+    break;
+
+    case 'last quarter':
+    range = Duration(days:90);
+    break;
+
+    default:
+    return[];
+  }
+
+  DateTime fromDate = now.subtract(range);
+
+  return mobCustData.where((data) 
+  {
+    String create = data['lcdcreatedon'].toString();
+    DateTime? date = DateTime.tryParse(create);
+    return date != null && date.isAfter(fromDate);
+  }).toList();
+
+  }
